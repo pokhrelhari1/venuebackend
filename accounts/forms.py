4bf2ae66ec6dbdabd658ce0c75a7909a1ffbaef1
Pratima84 <pratimachaudhary84@gmail.com>
2020-12-14 16:05:32 +0545

    Login/registartion through mysql

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,16 @@
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
 
 class CreateUserFrom(UserCreationForm): #replica of default form
+    username= forms.CharField(max_length= 100, help_text="First Name")
     first_name= forms.CharField(max_length= 100, help_text="First Name")
     last_name= forms.CharField(max_length= 100, help_text="First Name")
     email = forms.EmailField(max_length=150, help_text='Email')
     address= forms.CharField(max_length= 100, help_text="Address")
-    contact= forms.IntegerField(required=True) 
+    contact= forms.IntegerField() 
 
     class Meta:
         model = User
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -10,11 +11,11 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=150, help_text='Email')
     address = models.CharField(max_length=150)
-    contact = models.IntegerField(null=True)
+    contact = models.IntegerField(null= True, blank= True )
    
 
     def __str__(self):
-        return self.user.username
+        return self.first_name
 
 @receiver(post_save, sender=User)
 def update_profile_signal(sender, instance, created, **kwargs):
